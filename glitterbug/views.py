1ec33c979ed563354a378770f42818290eefac38
iamshabeeb <shabeebshaz105101@gmail.com>
2022-07-30 19:45:23 +0530

    errorrr

--- glitterbug/views.py
+++ glitterbug/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from store.models import Product,ReviewRating
 
 def home (request):
+    reviews = 0
     products=Product.objects.filter(is_available=True).order_by('created_date')
     for product in products:
         reviews = ReviewRating.objects.filter(product_id = product.id, status = True)
