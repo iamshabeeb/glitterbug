ead12e3536687bc0ca3ce9cdf87843d0115cf605
iamshabeeb <shabeebshaz105101@gmail.com>
2022-07-30 19:31:24 +0530

    last

--- glitterbug/settings.py
+++ glitterbug/settings.py
@@ -91,9 +91,9 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Glitterbug',
-        'USER': 'postgres',
-        'PASSWORD':'iamshabi101',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST':'localhost'
     }
 }
@@ -172,6 +172,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 
-AWS_ACCESS_KEY_ID = 'AKIATTQ7BENAEBBTHAHG'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'glitterbug-bucket'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
